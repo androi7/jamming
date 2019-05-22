4655fbb43eea0a46bfef3e549e9a40666eac512f
Androi <gabriel.hilti@gmail.com>
2019-05-22 19:07:35 +0200

    Updates

---
+++ precache-manifest.34635f79cab8199860d3fc3679585e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a010d1579f0904e8c6ec8e3b484041d6",
+    "url": "/jamming/index.html"
+  },
+  {
+    "revision": "a5adf3229d390b1a9565",
+    "url": "/jamming/static/css/main.15894dd5.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/jamming/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "a5adf3229d390b1a9565",
+    "url": "/jamming/static/js/main.4e7fe2fb.chunk.js"
+  },
+  {
+    "revision": "c3d7dfc925f613b07d70",
+    "url": "/jamming/static/js/runtime~main.6e9a7a58.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jamming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
